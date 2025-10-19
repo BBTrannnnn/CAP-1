1498e5487d880bc0613259d1258f6eacbba6cada
Tên của bạn <hungnguyen23072004@gmail.com>
2025-10-19 15:13:18 +0700

    sưa role

--- BE/src/controllers/User_controller.js
+++ BE/src/controllers/User_controller.js
@@ -87,7 +87,7 @@ const register = asyncHandler(async (req, res) => {
     });
   }
 
-  const { name, phone, email, password, confirmPassword } = req.body;
+  const { name, phone, email, password,role, confirmPassword } = req.body;
   
   // Kiểm tra email đã tồn tại
   const existingUser = await User.findOne({ email });
@@ -131,6 +131,7 @@ const register = asyncHandler(async (req, res) => {
         name: user.name,
         email: user.email,
         phone: user.phone,
+        role: user.role,
         isActive: user.isActive,
         loginProvider: user.loginProvider,
         createdAt: user.createdAt,
